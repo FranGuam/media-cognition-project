a18ed25136d2e4a11295aa5fa4e95a7fc2c46628
FranGuam <frankguansy@icloud.com>
2023-12-04 16:22:26 +0800

    feat: Robot Arm

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # media-cognition-project
+
 Class project for Media and Cognition 2023 Fall
+
+### 机械臂相关数据
+
+`coords = [x, y, z, rx, ry, rz]`
+
+面向屏幕
+- x：前后，远离屏幕为正，最大为284，最小为-284 
+- y：左右，左为正，最大为290，最小为-290
+- z：上下，上为正，最大为410，桌面为70 
+- rx：yz面，上为0，屏幕前往后看顺时针
+- ry：xz面，下为0，从左往右看逆时针
+- rz：xy面，远离屏幕为0，从上向下看逆时针
---
+++ robot.py
@@ -0,0 +1,114 @@
+from pymycobot.mycobot import MyCobot
+import time
+
+BOX_LEVEL = 105
+LEFT_BORDER = 75
+RIGHT_BORDER = -35
+NEAR_BORDER = 135
+FAR_BORDER = 215
+LEFT_PIXEL = 0
+RIGHT_PIXEL = 256
+NEAR_PIXEL = 256
+FAR_PIXEL = 0
+
+MODE = 0
+
+POSITION = {
+    "left-near":
+        [[120, 170, 240, -180, 0, 0],  # up
+         [120, 170, 175, -180, 0, 0]],  # down
+    "left-far":
+        [[200, 120, 230, -180, 0, 0],  # up
+         [215, 150, 220, -150, 0, -60]],  # down
+    "right-near":
+        [[120, -150, 240, -180, 0, 0],  # up
+         [120, -150, 175, -180, 0, 0]],  # down
+    "right-far":
+        [[200, -100, 235, -180, 0, 0],  # up
+         [220, -140, 220, -150, -10, -80]],  # down
+}
+
+mc = MyCobot('COM3', 115200)
+
+
+def cmp(pos, target):
+    if len(pos) == 0:
+        return 999
+    delta_x = abs(pos[0] - target[0])
+    delta_y = abs(pos[1] - target[1])
+    delta_z = abs(pos[2] - target[2])
+    delta = (delta_x ** 2 + delta_y ** 2 + delta_z ** 2) ** 0.5
+    return delta
+
+
+def move(x, y, z, *args, **kwargs):
+    if len(args) > 0:
+        coords = [x, y, z, *args]
+    else:
+        coords = [x, y, z, 180, 0, 180]
+    if "speed" in kwargs:
+        speed = kwargs["speed"]
+    else:
+        speed = 40
+    mc.send_coords(coords, speed, MODE)
+    start = time.time()
+    while cmp(mc.get_coords(), (x, y, z)) > 20:
+        if cmp(mc.get_coords(), (x, y, z)) == 999:
+            time.sleep(2)
+            break
+        assert time.time() - start < 3
+        time.sleep(0.1)
+    time.sleep(0.5)
+    return
+
+
+def pump_on():
+    mc.set_basic_output(2, 0)
+    mc.set_basic_output(5, 0)
+    time.sleep(2)
+    return
+
+
+def pump_off():
+    mc.set_basic_output(2, 1)
+    mc.set_basic_output(5, 1)
+    time.sleep(4)
+    return
+
+
+def init():
+    pump_off()
+    mc.send_angles([0, 0, 0, 0, 0, 0], 40)
+    time.sleep(3)
+    return
+
+
+def pixel_to_coord(pixel_x, pixel_y):
+    x = FAR_BORDER - (pixel_x - FAR_PIXEL) / (NEAR_PIXEL - FAR_PIXEL) * (FAR_BORDER - NEAR_BORDER)
+    y = LEFT_BORDER - (pixel_y - LEFT_PIXEL) / (RIGHT_PIXEL - LEFT_PIXEL) * (LEFT_BORDER - RIGHT_BORDER)
+    print(x, y)
+    return x, y
+
+
+def grasp(pos):
+    init()
+    move(pos[0], pos[1], BOX_LEVEL + 30)
+    move(pos[0], pos[1], BOX_LEVEL, speed=10)
+    pump_on()
+    move(pos[0], pos[1], BOX_LEVEL + 30, speed=10)
+    return
+
+
+def put_off(label):
+    move(*(POSITION[label][0]))
+    move(*(POSITION[label][1]))
+    pump_off()
+    move(*(POSITION[label][0]))
+    return
+
+
+if __name__ == "__main__":
+    init()
+    # Take a picture
+    grasp(pixel_to_coord(128, 128))
+    put_off("left-near")
