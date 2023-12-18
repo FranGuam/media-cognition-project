99f152de589b232d533c1b0a6235c57301e73ec4
xiangmy21 <2248278431@qq.com>
2023-12-18 18:38:21 +0800

    feat(match): use chinese clip

--- README.md
+++ README.md
@@ -28,3 +28,29 @@ Class project for Media and Cognition 2023 Fall
   pip install git+https://github.com/openai/CLIP.git
   ```
 
+### Chinese-Clip
+
+[OFA-Sys/Chinese-CLIP: Chinese version of CLIP which achieves Chinese cross-modal retrieval and representation generation. (github.com)](https://github.com/OFA-Sys/Chinese-CLIP)
+
+- 环境配置
+
+  ```
+  python >= 3.6.4
+  pytorch >= 1.8.0 (with torchvision >= 0.9.0)
+  CUDA Version >= 10.2
+  ```
+
+  ```
+  numpy
+  tqdm
+  six
+  timm
+  patch_ng
+  lmdb==1.3.0
+  torch>=1.7.1
+  torchvision
+  
+  cn_clip
+  ```
+
+  
--- match.py
+++ match.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import numpy as np
 import torch
-import clip
+import cn_clip.clip as clip
+from cn_clip.clip import load_from_name
 
 CONFIDENCE = 0.75
 
@@ -13,12 +14,14 @@ def arrayToImage(array: np.ndarray):
 
 
 def imageTextMatch(image: Image.Image, text: list[str]):
-    device = "cpu"
-    model, preprocess = clip.load("ViT-B/32", device=device)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(device)
+    model, preprocess = load_from_name("ViT-B-16", device=device, download_root='./')
+    model.eval()
     text = clip.tokenize(text).to(device)
     image = preprocess(image).unsqueeze(0).to(device)
     with torch.no_grad():
-        logits_per_image, logits_per_text = model(image, text)
+        logits_per_image, logits_per_text = model.get_similarity(image, text)
         probs = logits_per_image.softmax(dim=-1).cpu().numpy()
     return probs
 
